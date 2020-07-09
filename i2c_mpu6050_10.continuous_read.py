a4f75eeb688c62e5f4266588f12f4868e366564d
toefuVox <tb12e053.tkb@gmail.com>
2020-07-09 20:11:26 +0900

    Implemented the ability to send accelerometer information

--- i2c_mpu6050_10.continuous_read.py
+++ i2c_mpu6050_10.continuous_read.py
@@ -1,88 +1,112 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import smbus            # use I2C
+import smbus  # use I2C
+import requests
 import math
 from time import sleep  # time module
 
 ### define #############################################################
-DEV_ADDR = 0x68         # device address
-PWR_MGMT_1 = 0x6b       # Power Management 1
-ACCEL_XOUT = 0x3b       # Axel X-axis
-ACCEL_YOUT = 0x3d       # Axel Y-axis
-ACCEL_ZOUT = 0x3f       # Axel Z-axis
-TEMP_OUT = 0x41         # Temperature
-GYRO_XOUT = 0x43        # Gyro X-axis
-GYRO_YOUT = 0x45        # Gyro Y-axis
-GYRO_ZOUT = 0x47        # Gyro Z-axis
+DEV_ADDR = 0x68  # device address
+PWR_MGMT_1 = 0x6b  # Power Management 1
+ACCEL_XOUT = 0x3b  # Axel X-axis
+ACCEL_YOUT = 0x3d  # Axel Y-axis
+ACCEL_ZOUT = 0x3f  # Axel Z-axis
+TEMP_OUT = 0x41  # Temperature
+GYRO_XOUT = 0x43  # Gyro X-axis
+GYRO_YOUT = 0x45  # Gyro Y-axis
+GYRO_ZOUT = 0x47  # Gyro Z-axis
+
 
 # 1byte read
-def read_byte( addr ):
-    return bus.read_byte_data( DEV_ADDR, addr )
+def read_byte(addr):
+    return bus.read_byte_data(DEV_ADDR, addr)
+
 
 # 2byte read
-def read_word( addr ):
-    high = read_byte( addr   )
-    low  = read_byte( addr+1 )
+def read_word(addr):
+    high = read_byte(addr)
+    low = read_byte(addr + 1)
     return (high << 8) + low
 
+
 # Sensor data read
-def read_word_sensor( addr ):
-    val = read_word( addr )
-    if( val < 0x8000 ):
-        return val # positive value
+def read_word_sensor(addr):
+    val = read_word(addr)
+    if (val < 0x8000):
+        return val  # positive value
     else:
-        return val - 65536 # negative value
+        return val - 65536  # negative value
+
 
 # Get Temperature
 def get_temp():
-    temp = read_word_sensor( TEMP_OUT )
+    temp = read_word_sensor(TEMP_OUT)
     # offset = -521 @ 35℃
-    return ( temp + 521 ) / 340.0 + 35.0
+    return (temp + 521) / 340.0 + 35.0
+
 
 # Get Gyro data (raw value)
 def get_gyro_data_lsb():
-    x = read_word_sensor( GYRO_XOUT )
-    y = read_word_sensor( GYRO_YOUT )
-    z = read_word_sensor( GYRO_ZOUT )
-    return [ x, y, z ]
+    x = read_word_sensor(GYRO_XOUT)
+    y = read_word_sensor(GYRO_YOUT)
+    z = read_word_sensor(GYRO_ZOUT)
+    return [x, y, z]
+
+
 # Get Gyro data (deg/s)
 def get_gyro_data_deg():
-    x,y,z = get_gyro_data_lsb()
+    x, y, z = get_gyro_data_lsb()
     # Sensitivity = 131 LSB/(deg/s), @cf datasheet
     x = x / 131.0
     y = y / 131.0
     z = z / 131.0
-    return [ x, y, z ]
+    return [x, y, z]
+
 
 # Get Axel data (raw value)
 def get_accel_data_lsb():
-    x = read_word_sensor( ACCEL_XOUT )
-    y = read_word_sensor( ACCEL_YOUT )
-    z = read_word_sensor( ACCEL_ZOUT )
-    return [ x, y, z ]
+    x = read_word_sensor(ACCEL_XOUT)
+    y = read_word_sensor(ACCEL_YOUT)
+    z = read_word_sensor(ACCEL_ZOUT)
+    return [x, y, z]
+
+
 # Get Axel data (G)
 def get_accel_data_g():
-    x,y,z = get_accel_data_lsb()
+    x, y, z = get_accel_data_lsb()
     # Sensitivity = 16384 LSB/G, @cf datasheet
     x = x / 16384.0
     y = y / 16384.0
     z = z / 16384.0
     return [x, y, z]
 
+
 ### Main function ######################################################
-bus = smbus.SMBus( 1 )
-bus.write_byte_data( DEV_ADDR, PWR_MGMT_1, 0 )
+bus = smbus.SMBus(1)
+bus.write_byte_data(DEV_ADDR, PWR_MGMT_1, 0)
 while 1:
     temp = get_temp()
     print('t= %.2f' % temp, '\t', end='')
 
-    gyro_x,gyro_y,gyro_z = get_gyro_data_deg()
-    print('Gx= %.3f' % gyro_x, '\t',end='')
-    print('Gy= %.3f' % gyro_y, '\t',end='')
-    print('Gz= %.3f' % gyro_z, '\t',end='')
+    gyro_x, gyro_y, gyro_z = get_gyro_data_deg()
+    print('Gx= %.3f' % gyro_x, '\t', end='')
+    print('Gy= %.3f' % gyro_y, '\t', end='')
+    print('Gz= %.3f' % gyro_z, '\t', end='')
 
-    accel_x,accel_y,accel_z = get_accel_data_g()
-    print('Ax= %.3f' % accel_x, '\t',end='')
-    print('Ay= %.3f' % accel_y, '\t',end='')
+    accel_x, accel_y, accel_z = get_accel_data_g()
+    print('Ax= %.3f' % accel_x, '\t', end='')
+    print('Ay= %.3f' % accel_y, '\t', end='')
     print('Az= %.3f' % accel_z, '\t')
-    sleep( 1 )
+
+    webhoos_url = 'http://192.168.24.117:5000'
+    headers = {'from': 'i2c'}
+    sensor_data = {
+        'rot_x': gyro_y,
+        'rot_y': gyro_y,
+        'rot_z': gyro_z,
+        'accel_x': accel_x,
+        'accel_y': accel_y,
+        'accel_z': accel_z
+    }
+    requests.post(webhoos_url, headers=headers, json=sensor_data)
+    sleep(1)
